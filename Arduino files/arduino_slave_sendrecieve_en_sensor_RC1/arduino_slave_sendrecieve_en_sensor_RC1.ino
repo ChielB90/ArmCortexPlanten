25624cc5e65b22366328bfd6cefd26ec2da1635b
KayaTables <33062528+KayaTables@users.noreply.github.com>
2017-10-24 16:47:59 +0200

    Arduino I2C en Sensor read files geüpload

---
+++ Arduino files/arduino_slave_sendrecieve_en_sensor_RC1/arduino_slave_sendrecieve_en_sensor_RC1.ino
@@ -0,0 +1,22 @@
+#define SENSORPIN 5
+#define SLAVE_ADDRESS 20 
+
+char recieveBuff[256]; //voor de I2CRecieve interrupt
+
+union I2C {
+  float f;
+  byte* I2Cdata;
+}; //omdat de i2c send functie een groep bytes verwacht
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.print("startI2C returns: ");
+  Serial.println(startI2C(SLAVE_ADDRESS));
+}
+
+void loop() {
+  prepareI2Cdata(checkHumidity());
+  
+  
+}
---
+++ Arduino files/arduino_slave_sendrecieve_en_sensor_RC1/checkHumidity.ino
@@ -0,0 +1,29 @@
+/* deze functie checkt een millisecone lang hoeveel statechanges
+ * er zijn op de pin. gedeelt door twee om van de state changes
+ * aantal periodes te maken en keer duizend om er het aantal 
+ * periodes per seconde van te maken.
+ * de functie returnt een float. de ferquentie.
+ * SENSORPIN moet in de main file gedefinet worden.
+ */
+
+float checkHumidity()
+{
+  float count = 0;
+  bool pinState = digitalRead(SENSORPIN);
+  
+  for(int i = micros()+1000; i > micros;) 
+  {
+    if (pinState != digitalRead(SENSORPIN)) count++;
+    
+    pinState = digitalRead(SENSORPIN);
+  }
+
+  Serial.print("frequency is: ");
+  Serial.println(count * 500);
+  
+  return count * 500;
+
+  
+}
+
+
