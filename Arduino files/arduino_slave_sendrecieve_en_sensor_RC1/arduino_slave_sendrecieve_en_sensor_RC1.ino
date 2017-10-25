9533c471a13c6bdf18d05b1686d9da7123db7e8b
KayaTables <33062528+KayaTables@users.noreply.github.com>
2017-10-25 12:04:57 +0200

    digitalread vervangen door een sneller mechanisme

--- Arduino files/arduino_slave_sendrecieve_en_sensor_RC1/arduino_slave_sendrecieve_en_sensor_RC1.ino
+++ Arduino files/arduino_slave_sendrecieve_en_sensor_RC1/arduino_slave_sendrecieve_en_sensor_RC1.ino
@@ -8,11 +8,17 @@ union I2C {
   byte I2Cdata[4];
 }; //omdat de i2c send functie een groep bytes verwacht
 
+uint8_t SensorPin_mask;
+volatile uint8_t *SensorPin_port;
 
 void setup() {
   Serial.begin(9600);  //bij voorkeur maak ik deze sneller (bijv 250000)
   Serial.print("startI2C returns: ");
   Serial.println(startI2C(SLAVE_ADDRESS));
+
+  SensorPin_mask = digitalPinToBitMask(SENSORPIN);
+  SensorPin_port = portInputRegister(digitalPinToPort(SENSORPIN));
+
 }
 
 void loop() {
--- Arduino files/arduino_slave_sendrecieve_en_sensor_RC1/checkHumidity.ino
+++ Arduino files/arduino_slave_sendrecieve_en_sensor_RC1/checkHumidity.ino
@@ -9,19 +9,26 @@
 float checkHumidity() //checkDankness of checkDank zou beter kloppen
 {
   float count = 0;
-  bool pinState = digitalRead(SENSORPIN);
+  int pinState = (*SensorPin_port & SensorPin_mask);
+  //int pinState = digitalRead(SENSORPIN);
   
-  for(int i = micros()+1000; i > micros(); i = micros()) 
-  {
-    if (pinState != digitalRead(SENSORPIN)) count++;
+  //unsigned long test1 = micros();
     
-    pinState = digitalRead(SENSORPIN);
+  for(unsigned long i = micros() + 1000; i > micros();) 
+  {
+    if (pinState != (*SensorPin_port & SensorPin_mask)) count++;
+    //if (pinState != digitalRead(SENSORPIN)) count++;
+    pinState = (*SensorPin_port & SensorPin_mask);
+    //pinState = digitalRead(SENSORPIN);
   }
 
-  //Serial.print("frequency is: ");
-  //Serial.println(count * 500);
+  //unsigned long test2 = micros();
+
+  Serial.print("frequency is: ");
+  Serial.println(count * 500);
+
+  //Serial.println(test1);
+  //Serial.println(test2);
   
   return count * 500; 
 }
-
-
