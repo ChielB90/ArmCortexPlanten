53a108286e2fc340eb5234f0d4e74ea7d031f57d
KayaTables <33062528+KayaTables@users.noreply.github.com>
2017-10-25 14:59:05 +0200

    Add files via upload

--- Arm Cortex files/ports/I2C_init.c
+++ Arm Cortex files/ports/I2C_init.c
@@ -25,20 +25,20 @@ void I2Cinit(void) {
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_I2C1);	// SCL
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_I2C1); // SDA
 
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7; // we are going to use PB6 and PB7
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;			// set pins to alternate function
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;		// set GPIO speed
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_OD;			// set output to open drain --> the line has to be only pulled low, not driven high
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;			// enable pull up resistors
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7; // we gebruiken PB6 en PB7
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;			// de pins worden alternate function
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;		// GPIO speed
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_OD;			// de output moet open drain zijn
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;			// enable pull up
 	GPIO_Init(GPIOB, &GPIO_InitStruct);					// init GPIOB
 
 	// configure I2C1
 	I2C_InitStruct.I2C_ClockSpeed = 100000; 		// 100kHz
-	I2C_InitStruct.I2C_Mode = I2C_Mode_I2C;			// I2C mode
-	I2C_InitStruct.I2C_DutyCycle = I2C_DutyCycle_2;	// 50% duty cycle --> standard
-	I2C_InitStruct.I2C_OwnAddress1 = 0x00;			// own address, not relevant in master mode
-	I2C_InitStruct.I2C_Ack = I2C_Ack_Disable;		// disable acknowledge when reading (can be changed later on)
-	I2C_InitStruct.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit; // set address length to 7 bit addresses
+	I2C_InitStruct.I2C_Mode = I2C_Mode_I2C;			// I2C modus
+	I2C_InitStruct.I2C_DutyCycle = I2C_DutyCycle_2;	// 50% duty cycle
+	I2C_InitStruct.I2C_OwnAddress1 = 0x00;			// master heeft 0 als adres
+	I2C_InitStruct.I2C_Ack = I2C_Ack_Disable;		// ACK is uit
+	I2C_InitStruct.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit; // 7 bits adressen
 	I2C_Init(I2C1, &I2C_InitStruct);				// init I2C1
 
 	// enable I2C1
