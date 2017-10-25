b36916f31a97f038f8b32e4fef8e7111fa7ffaf5
KayaTables <33062528+KayaTables@users.noreply.github.com>
2017-10-25 16:21:48 +0200

    ik kan niet compileren dus who knows

--- sensor_mutex.c
+++ sensor_mutex.c
@@ -1,19 +1,35 @@
 //////////////////////////////////////////////////////////////////////////////
 // prog: sensor_mutex.c
-//       shows the usage of mutual exclusion semaphores or 'mutexes'
+
 // note: for OSMboxCreate, see main.c!
+// comm: I2C protocol is used for communication with the sensors.
 // auth: CB
 //////////////////////////////////////////////////////////////////////////////
+
+
 
 #include <includes.h>
 #include "main.h"
 
-void getData()
+#define SLAVE_ADDRESS 20
+
+void getData(char address)
 {
-	DATA = 1;
+	address++ = address << 1; 			        //bitshift address naar links en vul de lege bit met een een voor READ
+	I2C_GenerateSTART (I2C1, ENABLE); 		    //stuur START
+	I2C_Send7bitAddress (I2C1, address +1); 	//stuur adres
+	I2C_ReceiveData (I2C1);	 				    //leest de i2c buffer uit MISSCHIEN IS DIE NIET ZO SNEL VOL PAS OP
+	
 }
 
 void Sensor_Task(void *pdata)
 {
-	getData();
+	I2Cinit();
+	
+	while(1){
+		getData(SLAVE_ADDRESS);
+		
+		OSTimeDly(LOOP_DELAY);
+	}
+	
 }
